ab3d1a4d468749b97cd039d61afefa55ee39d6e0
busraakay <61781887+busraakay@users.noreply.github.com>
2021-04-05 13:54:25 +0300

    NDPProje

---
+++ NDPProje/Ordek.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace NDPProje
+{
+    class Ordek : Hayvan  //Hayvan sınıfından kalıtım aldırılır.
+    {
+        SoundPlayer ses = new SoundPlayer();//Ses için oluşturulur.
+
+        private int ordekYumurtası;  //Yumurta adeti için.
+        string ordekSes = Application.StartupPath + "\\sesler\\ordek.wav";//Ses dosyasının seçmek için.
+
+        public override int YemSat()// Yemi gerekli işlem yaparak kasaya aktarır.
+        {
+          
+             Kasa += ordekYumurtası * 3;
+            return ordekYumurtası = 0;
+        }
+
+        public override int YemUret()// Gerekli işlem yapılarak saniyeye göre yumurta artırılır.
+        {
+            Saniye++;
+            if(Saniye%5==0)
+            {
+                ordekYumurtası++;
+            }
+            return ordekYumurtası;
+        }
+
+        public override void SesVer()//Sesin konumu belli edilerek ses çaldırılır.
+        {
+            ses.SoundLocation = ordekSes;
+            ses.Play();
+        }
+
+    }
+}
---
+++ NDPProje/Tavuk.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace NDPProje
+{
+    class Tavuk : Hayvan  //Hayvan sınıfından kalıtım aldırılır.
+    {
+        SoundPlayer ses = new SoundPlayer();    //Ses için oluşturulur.
+
+        private int tavukYumurtası = 0;        //Yumurta adeti için.
+        private string tavukSes = Application.StartupPath + "\\sesler\\tavuk.wav"; //Ses dosyasının seçmek için.
+
+
+        public override int YemSat()// Yemi gerekli işlem yaparak kasaya aktarır.
+        {
+            Kasa += tavukYumurtası * 1;
+            return tavukYumurtası = 0;
+        }
+
+        
+        
+        public override int YemUret()// Gerekli işlem yapılarak saniyeye göre yumurta artırılır.
+        {
+            Saniye++;
+            if (Saniye % 3 == 0)
+            {
+                tavukYumurtası++;
+            }
+
+            return tavukYumurtası;
+
+        }
+        public override void SesVer()//Sesin konumu belli edilerek ses çaldırılır.
+        {
+            ses.SoundLocation = tavukSes;
+            ses.Play();
+        }
+
+
+    }
+}
